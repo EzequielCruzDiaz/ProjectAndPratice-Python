6f158d76efee5cc2360accce69ac20862b42b06d
ezequiel <ezequielcruz018@gmail.com>
2024-06-26 17:30:10 -0400

    Info

---
+++ Ternario.py
@@ -0,0 +1,10 @@
+skills = "yes"
+#
+# if skills == "yes":
+#     message = "capable"
+# else:
+#     message = "uncapable"
+
+
+message = "capable" if skills == "yes" else "Uncapable"
+print(message)
