15f27de14f04d9c2af85c8449df037245fa50091
ezequiel <ezequielcruz018@gmail.com>
2024-07-22 15:45:30 -0400

    divisas

--- Divisas.py
+++ Divisas.py
@@ -1,6 +1,16 @@
 import datetime
 
-nombre = input("Introduzca su nombre")
-
+nombre = input("Introduzca su nombre: ")
 fecha = datetime.datetime.now().strftime("%Y-%m-%d")
+hora = datetime.datetime.now().hour
 
+
+if 5 <= hora < 12:
+    saludo = f"Buenos Dias {nombre}"
+elif 12 <= hora < 18:
+    saludo = f"Buenas Tardes {nombre}"
+else:
+    saludo = f"Buenas Noches {nombre}"
+
+
+print(f'{saludo} tu transaccion fue realizada el {fecha} ')
